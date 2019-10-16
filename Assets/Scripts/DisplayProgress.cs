9ffd2f15b6fc6efdf0985b7c67b7319f7e9944eb
FredericRP <frederic@rolland-porche.fr>
2019-10-17 01:05:09 +0200

    - NEW : first commit

---
+++ Assets/Scripts/DisplayProgress.cs
@@ -0,0 +1,17 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class DisplayProgress : MonoBehaviour
+{
+  [SerializeField]
+  LoadScene sceneLoader;
+  [SerializeField]
+  TextMeshProUGUI text;
+
+  // Update is called once per frame
+  void Update()
+  {
+    // P0 format displays wrong char, must investigate
+    text.text = (100*sceneLoader.Progress).ToString("F0")+ " %";
+  }
+}
---
+++ Assets/Scripts/LoadScene.cs
@@ -0,0 +1,26 @@
+﻿using FredericRP.EventManagement;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadScene : MonoBehaviour
+{
+  [SerializeField]
+  string sceneName;
+  [SerializeField]
+  bool async = false;
+
+  AsyncOperation asyncOperation;
+  public float Progress { get => asyncOperation != null ? asyncOperation.progress : 0; }
+  public bool Done { get => asyncOperation != null ? asyncOperation.isDone : true; }
+
+  // Start is called before the first frame update
+  void Start()
+  {
+    if (async)
+      asyncOperation = SceneManager.LoadSceneAsync(sceneName);
+    else
+      SceneManager.LoadScene(sceneName);
+  }
+}
