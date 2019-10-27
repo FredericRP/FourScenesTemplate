20dffb01b53b3b59eaae827f6403593ea66c1b55
FredericRP <frederic@rolland-porche.fr>
2019-10-27 21:56:21 +0100

    - NEW : loading progress game event with int parameter - NEW : scene loaded auto handling to hide transition - MOD : scripts clean up - MOD : tips and transition moved to previous scene for better user display (small memory footprint)

---
+++ Assets/FredericRP/Template/Scripts/LoadScene.cs
@@ -0,0 +1,98 @@
+﻿using FredericRP.EventManagement;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace FredericRP.ProjectTemplate
+{
+  public class LoadScene : MonoBehaviour
+  {
+    /// <summary>
+    /// Loading process can be called: by a script, onStart automatically, on specified event
+    /// </summary>
+    protected enum LoadingTrigger { onCall, onStart, onEvent};
+
+    [SerializeField]
+    string sceneName = null;
+    [SerializeField]
+    GameEvent loadingProgressEvent = null;
+    [SerializeField]
+    protected bool async = false;
+    /// <summary>
+    /// When will be called the loading process ? if onCall, it's explicitly from a script
+    /// </summary>
+    [SerializeField]    
+    protected LoadingTrigger loadingTrigger = LoadingTrigger.onStart;
+    [SerializeField]
+    protected GameEvent loadingEvent;
+
+    [SerializeField]
+    float delayToUpdateProgress = 0.2f;
+
+    protected AsyncOperation asyncOperation;
+    protected float nextProgressUpdateTime;
+
+    protected int progress;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+      nextProgressUpdateTime = 0;
+      progress = 0;
+      if (loadingTrigger == LoadingTrigger.onStart)
+        StartLoading();
+    }
+
+    private void OnEnable()
+    {
+      SceneManager.sceneLoaded += OnSceneLoaded;
+      if (loadingTrigger == LoadingTrigger.onEvent)
+        EventHandler.AddEventListener(loadingEvent, StartLoading);
+    }
+
+    void OnDisable()
+    {
+      SceneManager.sceneLoaded -= OnSceneLoaded;
+      if (loadingTrigger == LoadingTrigger.onEvent)
+        EventHandler.RemoveEventListener(loadingEvent, StartLoading);
+    }
+
+    protected virtual void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+      if (loadingTrigger == LoadingTrigger.onEvent && loadingEvent != null)
+        EventHandler.TriggerEvent(loadingEvent);
+
+      Debug.Log(Time.time + ":" + gameObject.name + " > Scene Loaded !");
+    }
+
+    public virtual void StartLoading()
+    {
+      if (async)
+      {
+        Debug.Log(Time.time + ":" + gameObject.name + " >Loading ASYNC scene " + sceneName);
+        asyncOperation = SceneManager.LoadSceneAsync(sceneName);
+      }
+      else
+      {
+        Debug.Log(Time.time + ":" + gameObject.name + " >Loading scene " + sceneName);
+        SceneManager.LoadScene(sceneName);
+      }
+    }
+
+    protected void SendProgressEvent(int progress)
+    {
+      EventHandler.TriggerEvent<int>(loadingProgressEvent, progress);
+    }
+
+    private void Update()
+    {
+      if (Time.time > nextProgressUpdateTime && asyncOperation!=null)
+      {
+        nextProgressUpdateTime = Time.time + delayToUpdateProgress;
+        progress = Mathf.RoundToInt(asyncOperation.progress * 100);
+        SendProgressEvent(progress);
+      }
+    }
+  }
+}
---
+++ Assets/FredericRP/Template/Scripts/UI.LoadingProgress/DisplayProgress.cs
@@ -0,0 +1,31 @@
+﻿using FredericRP.EventManagement;
+using TMPro;
+using UnityEngine;
+
+namespace FredericRP.ProjectTemplate
+{
+  public class DisplayProgress : MonoBehaviour
+  {
+    [SerializeField]
+    IntGameEvent progressEvent = null;
+    [SerializeField]
+    TextMeshProUGUI text = null;
+
+    private void OnEnable()
+    {
+      EventHandler.AddEventListener<int>(progressEvent, UpdateProgress);
+    }
+
+    private void OnDisable()
+    {
+      EventHandler.RemoveEventListener<int>(progressEvent, UpdateProgress);
+    }
+
+    // Update is called once per frame
+    void UpdateProgress(int value)
+    {
+      // P0 format displays wrong char, must investigate
+      text.text = value.ToString("F0") + " %";
+    }
+  }
+}
